42c3e42f73c7a71bb0e25cb99bed111c423d04a9
olatzaranzabe <olatzaranzabe@gmail.com>
2020-02-11 20:51:21 +0100

    xx

--- modules/6-webapp/parcel/index.js
+++ modules/6-webapp/parcel/index.js
@@ -1,7 +1,23 @@
 import './styles.css'
 
-const root = document.querySelector('#root')
-let counter = 0
-setInterval(() => {
-    root.innerHTML = counter++
-}, 1000)
+// const root = document.querySelector('#root')
+// let counter = 0
+// setInterval(() => {
+//     root.innerHTML = counter++
+// }, 1000)
+
+const url = 'https://cors-anywhere.herokuapp.com/https://xkcd.com/info.0.json'
+
+async function getComicUrl() {
+    const response = await fetch(url, {})
+    const comic = await response.json()
+    return comic.img
+}
+
+async function findImg() {
+    const img = await getComicUrl()
+
+    document.querySelector('.main-img').setAttribute('src', img)
+}
+
+findImg()
