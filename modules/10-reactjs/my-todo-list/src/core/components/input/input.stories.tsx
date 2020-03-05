94878ff7cf54309ea7fccca6397cd1e004a7adbd
Olatz Aranzabe <olatzaranzabe@MacBook-Pro-de-Olatz.local>
2020-03-05 19:03:14 +0100

    wip input paint always with required

--- modules/10-reactjs/my-todo-list/src/core/components/input/input.stories.tsx
+++ modules/10-reactjs/my-todo-list/src/core/components/input/input.stories.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Input } from './input'
 
 export default {
   title: 'Input',
   component: Input
 }
-
-export const primary = () => <Input theme="primary" placeholder="write here"></Input>
-export const secondary = () => <Input theme="secondary"></Input>
+const WithState: React.FC<{isRequired: boolean}> = ({isRequired}) => {
+  const [value, setValue] = useState('')
+  return (
+    <>
+      <Input required={isRequired} value={value} label="My input" onChange={setValue}></Input>
+    </>
+  )
+}
+export const base = ()=> < WithState isRequired={false} />
+export const required = ()=> < WithState isRequired={true} />
--- modules/10-reactjs/my-todo-list/src/core/components/input/input.tsx
+++ modules/10-reactjs/my-todo-list/src/core/components/input/input.tsx
@@ -4,21 +4,33 @@ import styles from './input.module.css'
 
 const cx = bind(styles)
 
-interface Props extends React.HTMLProps<HTMLInputElement> {
-    theme?: 'primary' | 'secondary'
+interface Props  {
+    value: string
+    label: string
+    onChange(value: string): void
+    required?: boolean
   }
   
+ 
   export const Input: React.FunctionComponent<Props> = ({
-      children,
-      className,
-      theme,
-      ...rest
+      onChange,
+      label,
+      value,
+      required
     }) => {
       return (
-        <input
-          className={cx('input', theme, className)}
-          {...rest}
-        />
+          <label>
+              {label}{required ? '*' : ''}
+              <input
+                value={value}
+                className={cx('input', {required})}
+                onChange={event => onChange(event.target.value)}
+            />
+            <span
+            className={cx('hidden', {show: value.length === 0 })}
+            >Campo requerido</span>
+          </label>
+        
 
       )
     };
